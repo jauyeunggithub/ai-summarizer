afa248e87799a57821009ddb40ab199675aad471
Ho Hang Au Yeung <mayeung@telus.net>
2025-05-17 18:33:13 -0700

    replace db url

--- backend/app.py
+++ backend/app.py
@@ -3,8 +3,13 @@ from flask import Flask
 from flask_sqlalchemy import SQLAlchemy
 
 
+with open("/run/secrets/db_password.txt", "r") as secret_file:
+    db_password = secret_file.read().strip()
+
+db_url = f"postgresql://postgres:{db_password}@db:5432/postgres"
+
 app = Flask(__name__)
-app.config['SQLALCHEMY_DATABASE_URI'] = os.getenv('DATABASE_URL')
+app.config['SQLALCHEMY_DATABASE_URI'] = db_url
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 db = SQLAlchemy(app)
 
