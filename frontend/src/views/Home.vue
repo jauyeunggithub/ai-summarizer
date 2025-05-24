765e5b8a6c1ad8ab4e5add13c1d0b8ed36fc18fc
Ho Hang Au Yeung <mayeung@telus.net>
2025-05-24 12:39:57 -0700

    add bootstrap

---
+++ frontend/src/views/Home.vue
@@ -0,0 +1,15 @@
+<template>
+  <TopBar />
+  <div></div>
+</template>
+
+<script>
+import TopBar from '@/components/TopBar.vue'
+
+export default {
+  name: 'HomePage',
+  components: {
+    TopBar,
+  },
+}
+</script>
