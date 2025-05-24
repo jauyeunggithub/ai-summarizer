84897379e550685a63f43ee644b39b40e7a748c3
Ho Hang Au Yeung <mayeung@telus.net>
2025-05-24 13:57:35 -0700

    add get price

--- frontend/src/views/Home.vue
+++ frontend/src/views/Home.vue
@@ -2,10 +2,22 @@
   <TopBar />
 
   <div class="p-5 mb-4 bg-success text-white">
-    <div class="container py-5">
+    <div class="container d-flex flex-column justify-content-center align-items-center">
       <h1 class="display-5 fw-bold">Never Read Boring Text Again</h1>
-      <p class="fs-4">Sign Up for Online AI Summarizer to get summaries of documents.</p>
-      <button class="btn btn-primary btn-lg" type="button">Sign Up Now</button>
+      <p class="fs-2">Sign Up for Online AI Summarizer to get summaries of documents.</p>
+      <button class="btn btn-primary btn-lg fs-2" type="button">Sign Up Now</button>
+    </div>
+  </div>
+
+  <div class="px-5 py-4">
+    <div class="container d-flex flex-column justify-content-center align-items-center">
+      <h1 class="display-5 fw-bold">For the price of ${{ price }}, you get:</h1>
+
+      <ul>
+        <li class="fs-2">Summarize .docx, .pdf, and audio files with AI</li>
+        <li class="fs-2">Download summaries generated</li>
+        <li class="fs-2">Private storage of files</li>
+      </ul>
     </div>
   </div>
 </template>
@@ -22,9 +34,15 @@ export default {
   data() {
     return { prices: [] }
   },
+  computed: {
+    price() {
+      const [subscriptionPrice] = this.prices
+      return (subscriptionPrice.unit_amount / 100).toFixed(2)
+    },
+  },
   async beforeMount() {
-    this.prices = await getActivePrices()
-    console.log(this.prices)
+    const { data } = await getActivePrices()
+    this.prices = data
   },
   methods: {},
 }
